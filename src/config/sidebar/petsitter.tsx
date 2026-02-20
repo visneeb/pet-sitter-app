248f1f582b1e37ffbcfd4d4566c785a46de4c4b1
VisneeB <visinee@gmail.com>
2026-02-20 21:09:19 +0700

    feat: implement initial UI structure including global styles, sidebar, form fields, and user/petsitter profile pages

---
+++ src/config/sidebar/petsitter.tsx
@@ -0,0 +1,20 @@
+import { SidebarConfig } from "@/types/sidebarType";
+
+export const petsitterSidebarItems: SidebarConfig = [
+  {
+    label: "Pet Sitter Profile",
+    href: "/petsitter-profile",
+  },
+  {
+    label: "Bookings List",
+    href: "/bookings-list",
+  },
+  {
+    label: "Calendar",
+    href: "/calendar",
+  },
+  {
+    label: "Payout Options",
+    href: "/payout-options",
+  },
+];
