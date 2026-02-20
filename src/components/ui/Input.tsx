400e630489e735d4317223d82132c9d1453bb386
Sorrawit Amdaeng <sorrawit.amdaeng@gmail.com>
2026-02-20 09:29:35 +0700

    feat: add Satoshi font family with multiple weights and update global styles for typography

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -10,7 +10,7 @@ export function Input({
       type={type}
       data-slot="input"
       className={cn(
-        "text-headline-2  text-black bg-white rounded-lg border border-gray-200 px-3",
+        "style-input text-black bg-white rounded-lg border border-gray-200 px-3",
         "placeholder:text-muted-foreground",
         "selection:bg-primary selection:text-primary-foreground transition-[color,box-shadow] outline-none ",
         "focus-visible:border  focus-visible:border-orange-500",
