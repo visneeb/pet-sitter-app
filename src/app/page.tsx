713afe3d58edccc7dcbfcbc0895022b859839efe
mrbuba <wwpptew101@gmail.com>
2026-02-20 14:37:51 +0700

    feat: update Navbar components for improved layout and responsive design, including menu toggle functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,7 @@ export default function Home() {
     <>
       <Navbar />
       <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <main className="flex gap-4">
-          <ActionButton variant="primary">Button </ActionButton>
-          <NavigationButton variant="primary" href="/">
-            Navigation Button
-          </NavigationButton>
-          <NavigationButton variant="secondary" href="/test">
-            Go to Test Page
-          </NavigationButton>
-        </main>
+        <main className="flex gap-4"></main>
       </div>
     </>
   );
--- src/components/common/nav-bar/NavbarGuest.tsx
+++ src/components/common/nav-bar/NavbarGuest.tsx
@@ -1,24 +1,76 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import logo from "@/assets/web-logo.png";
 import { NavigationButton } from "@/components/ui/Button";
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
 
 export default function NavbarGuest() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <div className="flex items-center justify-between bg-white h-[80px] px-[80px]">
+    <div className="relative flex items-center justify-between bg-white w-full md:h-[80px] px-[20px] md:px-[80px] style-body-2">
       <div>
         <Link href="/">
-          <Image src={logo} alt="Logo" />
+          <Image
+            src={logo}
+            alt="Logo"
+            className="h-[24px] md:h-[40px] w-auto"
+          />
         </Link>
       </div>
-      <div className="flex items-center gap-[16px] style-body-1">
-        <div className="px-[24px] py-[16px] text-black">
-          <Link href="/register-pet-sitter">Become a Pet Sitter</Link>
+      <div className="flex items-center gap-[12px] style-body-1">
+        <div className="hidden md:block px-[24px] py-[16px] text-black ">
+          <Link href="/register-pet-sitter" className="">
+            Become a Pet Sitter
+          </Link>
         </div>
-        <div className="px-[24px] py-[16px] text-black">
+        <div className="hidden md:block px-[24px] py-[16px] text-black">
           <Link href="/login">Login</Link>
         </div>
-        <div>
+        <div className="drawer md:hidden">
+          <input id="my-drawer-1" type="checkbox" className="drawer-toggle" />
+          <div
+            className="drawer-content"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            <label
+              htmlFor="my-drawer-1"
+              className=" drawer-button text-gray-300 flex items-center gap-3 px-4 py-3 hover:bg-gray-200 rounded-xl w-full cursor-pointer"
+            >
+              {isMenuOpen ? (
+                <X size={24} strokeWidth={2} />
+              ) : (
+                <Menu size={24} strokeWidth={2} />
+              )}
+            </label>
+          </div>
+          <div className="drawer-side top-[48px]">
+            <label
+              htmlFor="my-drawer-1"
+              aria-label="close sidebar"
+              className="drawer-overlay"
+            ></label>
+            <ul className="gap-[16px] px-[16px] py-[40px] menu bg-white h-full w-full p-4 ">
+              <li className="text-black style-body-1 hover:bg-gray-50">
+                <Link href="/register-pet-sitter">Become a Pet Sitter</Link>
+              </li>
+              <li className="text-black style-body-1 hover:bg-gray-50">
+                <Link href="/login">Login</Link>
+              </li>
+              <li className="w-full block">
+                <NavigationButton
+                  variant="primary"
+                  href="/search"
+                  className="w-full"
+                >
+                  Find A Pet Sitter
+                </NavigationButton>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="hidden md:block">
           <NavigationButton variant="primary" href="/search">
             Find A Pet Sitter
           </NavigationButton>
