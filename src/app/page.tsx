be37d0767d95cf41cb25484ba4baa97dffed03ca
mrbuba <wwpptew101@gmail.com>
2026-02-20 10:00:44 +0700

    feat: implement Navbar component with user and guest views, add web logo, and update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,24 @@
 import { ActionButton, NavigationButton } from "@/components/ui/Button";
 import { Input } from "@/components/ui/input/Input";
+import Navbar from "@/components/common/nav-bar/Navbar";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
-      <main className="flex gap-4">
-        <ActionButton variant="primary">Button </ActionButton>
-        <NavigationButton variant="primary" href="/">
-          Primary
-        </NavigationButton>
-        <NavigationButton variant="secondary" href="/test">
-          Go to Test Page
-        </NavigationButton>
+    <>
+      <Navbar />
+      <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
+        <main className="flex gap-4">
+          <ActionButton variant="primary">Button </ActionButton>
+          <NavigationButton variant="primary" href="/">
+            Primary
+          </NavigationButton>
+          <NavigationButton variant="secondary" href="/test">
+            Go to Test Page
+          </NavigationButton>
 
-        <Input type="name" placeholder="Place Holder"></Input>
-      </main>
-    </div>
+          <Input type="name" placeholder="Place Holder"></Input>
+        </main>
+      </div>
+    </>
   );
 }
