d12ae05d8211a069e643a5727e38a3bd81f42ef6
VisneeB <visinee@gmail.com>
2026-03-18 15:55:27 +0700

    style: improve styling of detail label

--- src/components/ui/detail/DetailLabel.tsx
+++ src/components/ui/detail/DetailLabel.tsx
@@ -7,8 +7,8 @@ export default function DetailLabel({
 }: LabelValueProps) {
   return (
     <div className={`flex flex-col gap-1 ${className}`}>
-      <h4 className="text-xl text-gray-300 font-bold">{label}</h4>
-      <span className="text-base text-black font-regular">{value}</span>
+      <h4 className="style-headline-4 text-gray-300">{label}</h4>
+      <span className="style-body-2 text-black">{value}</span>
     </div>
   );
 }
