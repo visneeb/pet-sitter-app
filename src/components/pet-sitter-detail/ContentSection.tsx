7aa6fec7cf5e42b9104b977015ed32d8b191f5cf
Sorrawit Amdaeng <sorrawit.amdaeng@gmail.com>
2026-03-19 13:42:15 +0700

    feat: integrate Markdown support in ContentSection and ReviewCard components for enhanced text formatting

--- src/components/pet-sitter-detail/ContentSection.tsx
+++ src/components/pet-sitter-detail/ContentSection.tsx
@@ -3,11 +3,16 @@ interface ContentSectionProps {
   children: React.ReactNode;
 }
 
-export default function ContentSection({ title, children }: ContentSectionProps) {
+export default function ContentSection({
+  title,
+  children,
+}: ContentSectionProps) {
   return (
     <div className="flex flex-col gap-3">
       <h3 className="style-headline-4 lg:style-headline-3">{title}</h3>
-      <div className="flex flex-col gap-3 text-gray-500 style-body-3 lg:style-body-2">{children}</div>
+      <div className="flex flex-col gap-3 text-gray-500 style-body-3 lg:style-body-2">
+        {children}
+      </div>
     </div>
   );
 }
